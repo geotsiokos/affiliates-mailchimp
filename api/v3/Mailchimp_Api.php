6a0c6adee675da0e4cc36d17a1df33331c2ed77b
geotsiokos <gtsiokos@gmail.com>
2018-04-29 17:26:38 +0300

    added wrappers for API URLs, formatting

--- api/v3/Mailchimp_Api.php
+++ api/v3/Mailchimp_Api.php
@@ -24,9 +24,25 @@ if ( !defined( 'ABSPATH' ) ) {
  */
 class Mailchimp_Api {
 
+	/**
+	 * Mailchimp API key
+	 *
+	 * @var string
+	 */
 	public $apikey;
-	public $ch;
+
+	/**
+	 * Mailchimp API root URL
+	 *
+	 * @var string
+	 */
 	public $root  = 'https://api.mailchimp.com/3.0';
+
+	/**
+	 * Debug
+	 *
+	 * @var string
+	 */
 	public $debug = false;
 
 	const LISTS = '/lists';
@@ -44,39 +60,68 @@ class Mailchimp_Api {
 	 */
 	public function __construct( $apikey = null, $opts = array() ) {
 		if ( !function_exists( 'curl_init' ) || !function_exists( 'curl_setopt' ) ) {
-			throw new \Exception( "cURL support is required, but can't be found.");
+			throw new \Exception( "cURL support is required, but can't be found." );
 		}
+
 		if ( !$apikey ) {
-			$apikey = getenv('MAILCHIMP_APIKEY');
-		}
-		
-		if (!$apikey) {
 			$apikey = $this->readConfigs();
 		}
-		
-		if (!$apikey) {
+
+		if ( !$apikey ) {
 			throw new Mailchimp_Error( 'You must provide a MailChimp API key' );
 		}
-		
+
 		$this->apikey = $apikey;
-		$dc           = "us1";
-		
-		if ( strstr( $this->apikey, "-" ) ) {
-			list( $key, $dc ) = explode( "-", $this->apikey, 2 );
+		$dc           = 'us1';
+
+		if ( strstr( $this->apikey, '-' ) ) {
+			list( $key, $dc ) = explode( '-', $this->apikey, 2 );
 			if ( !$dc ) {
-				$dc = "us1";
+				$dc = 'us1';
 			}
 		}
 
 		$this->root = str_replace( 'https://api', 'https://' . $dc . '.api', $this->root );
 		$this->root = rtrim( $this->root, '/' ) . '/';
 
-		if ( !isset($opts['timeout'] ) || !is_int( $opts['timeout'] ) ) {
+		if ( !isset( $opts['timeout'] ) || !is_int( $opts['timeout'] ) ) {
 			$opts['timeout'] = 600;
 		}
-		if ( isset($opts['debug'] ) ) {
+		if ( isset( $opts['debug'] ) ) {
 			$this->debug = true;
 		}
+	}
+
+	/**
+	 * Sets the Request URL based on the request
+	 *
+	 * @param string $type
+	 * @param array $params request parameters
+	 * @param array $list_params
+	 * @return boolean
+	 */
+	private function request_url( $type, $params = array(), $list_params = array() ) {
+		switch ( $type ) {
+			case 'get' :
+				$result = self::LISTS . '?' . http_build_query( $parameters );
+				break;
+			case 'update' :
+			case 'check' :
+				$result = self::LISTS . '/' . $list_params['list_id'] . self::MEMBERS . '/' . md5( $parameters['email_address'] );
+				break;
+			case 'new' :
+				$result = self::LISTS . '/' . $list_params['list_id'] . self::MEMBERS;
+				break;
+			case 'interest_categories' :
+				$result = self::LISTS . '/' . $list_params['list_id'] . self::INTEREST_CATEGORIES;
+				break;
+			case 'interest' :
+				$result = self::LISTS . '/' . $list_params['list_id'] . self::INTEREST_CATEGORIES . '/' . $list_params['interest_category_id'] . '/' . self::INTERESTS;
+				break;
+			default :
+				$result = false;
+		}
+		return $result;
 	}
 
 	/**
@@ -85,110 +130,142 @@ class Mailchimp_Api {
 	 * @param array $parameters
 	 * @return boolean|mixed
 	 */
-    public function get_lists( $parameters = array() ) {
-    	$lists_url = self::LISTS . '?' . http_build_query( $parameters );
-    	return $this->make_request( 'lists', $parameters, $lists_url );
-    }
+	public function get_lists( $parameters = array() ) {
+		$result = null;
+		$lists_url = $this->request_url( 'get', $parameters );
+		if ( $lists_url ) {
+			$result = $this->make_request( 'lists', $parameters, $lists_url );
+		}
+		return $result;
+	}
 
-    /**
-     * Check list for a given email
-     *
-     * @param string $list_id
-     * @param array $parameters
-     * @return NULL|boolean|mixed
-     */
-    public function check_list( $list_id = null, $parameters = array() ) {
-    	$result = null;
-    	if ( isset( $list_id ) && isset( $parameters['email_address'] ) ) {
-    		$members_url = self::LISTS . '/' . $list_id . self::MEMBERS . '/' . md5( $parameters['email_address'] );
-    		$result = $this->make_request( 'check', $parameters, $members_url );
-    	}
-    	return $result;
-    }
+	/**
+	 * Check list for existing subscriber
+	 *
+	 * @param array $list_parameters
+	 * @param array $parameters
+	 * @return NULL|boolean|mixed
+	 */
+	public function check_list( $list_parameters = array(), $parameters = array() ) {
+		$result = null;
+		if ( isset( $list_parameters['list_id'] ) && isset( $parameters['email_address'] ) ) {
+			$members_url = $this->request_url( 'check', $parameters, $list_parameters );
+			if ( $members_url ) {
+				$result = $this->make_request( 'check', $parameters, $members_url );
+			}
+		}
+		return $result;
+	}
 
-    /**
-     * Add a new subscriber
-     *
-     * @param string $list_id
-     * @param array $parameters
-     * @return NULL|boolean
-     */
-    public function new_subscriber( $list_id = null, $parameters = array() ) {
-    	$result = null;
-    	if ( isset( $list_id ) && isset( $parameters['email_address'] ) ) {
-    		$new_member_url = self::LISTS . '/' . $list_id . self::MEMBERS;
-    		$result = $this>make_request( 'add', $parameters, $new_member_url );
-    	}
-    	return $result;
-    }
+	/**
+	 * Add a new subscriber
+	 *
+	 * @param array $list_parameters
+	 * @param array $parameters
+	 * @return NULL|boolean|mixed
+	 */
+	public function new_subscriber( $list_parameters = array(), $parameters = array() ) {
+		$result = null;
+		if ( isset( $list_parameters['list_id'] ) && isset( $parameters['email_address'] ) ) {
+			$new_member_url = $this->request_url( 'new', $parameters, $list_parameters );
+			if ( $new_member_url ) {
+				$result = $this->make_request( 'add', $parameters, $new_member_url );
+			}
+		}
+		return $result;
+	}
 
-    /**
-     * Update an existing email - subscribe or unsubscribe
-     *
-     * @param string $list_id
-     * @param array $parameters
-     * @return NULL|boolean
-     */
-    public function update_subscriber( $list_id = null, $parameters = array() ) {
-    	$result = null;
-    	if ( isset( $list_id ) && isset( $parameters['email_address'] ) ) {
-    		$update_member_url = self::LISTS . '/' . $list_id . self::MEMBERS . md5( $parameters['email_address'] );
-    		$result = $this>make_request( 'update', $parameters, $update_member_url );
-    	}
-    	return $result;
-    }
+	/**
+	 * Update an existing email
+	 *
+	 * @param array $list_parameters
+	 * @param array $parameters
+	 * @return NULL|boolean|mixed
+	 */
+	public function update_subscriber( $list_parameters = array(), $parameters = array() ) {
+		$result = null;
+		if ( isset( $list_parameters['list_id'] ) && isset( $parameters['email_address'] ) ) {
+			$update_member_url = $this->request_url( 'update', $parameters, $list_parameters );
+			if ( $update_member_url ) {
+				$result = $this->make_request( 'update', $parameters, $update_member_url );
+			}
+		}
+		return $result;
+	}
 
-    // /lists/{list_id}/interest-categories/{interest_category_id}/interests
-    // URL parameters example
-    /* $url_parameters = array(
-     	'list_id'              => $list_id,
-     	'interest_category_id' => $interest_category_id,
-     	'interest_id'          => $interest_id
-     */
+	/**
+	 * Add a new Interest Category
+	 *
+	 * @param array $list_parameters
+	 * @param array $parameters
+	 * @return NULL|boolean|mixed
+	 */
+	public function add_interest_category( $list_parameters = array(), $parameters = array() ) {
+		$result = null;
+		if ( isset( $list_parameters['list_id'] ) ) {
+			if ( isset( $parameters['title'] ) && isset( $parameters['type'] ) ) {
+				$interest_category_url = $this->request_url( 'interest_category', $parameters, $list_parameters );
+				if ( $interest_category_url ) {
+					$result = $this->make_request( 'add', $parameters, $interest_category_url );
+				}
+			}
+		}
+		return $result;
+	}
 
-    /**
-     * Make a request to Mailchimp API
-     *
-     * @param string $request
-     * @param array $parameters
-     * @param string $api_path
-     * @return boolean|mixed
-     */
+	/**
+	 * Add new Interest
+	 *
+	 * @param array $list_parameters
+	 * @param array $parameters
+	 * @return NULL|boolean|mixed
+	 */
+	public function add_interest( $list_parameters = array(), $parameters = array() ) {
+		$result = null;
+		if ( isset( $list_parameters['list_id'] ) ) {
+			if ( isset( $list_parameters['interest_category_id'] ) && isset( $parameters['name'] ) ) {
+				$interest_url = $this->request_url( 'interest', $parameters, $list_parameters );
+				if ( $interest_url ) {
+					$result = $this->make_request( 'add', $parameters, $interest_url );
+				}
+			}
+		}
+		return $result;
+	}
+
+	/**
+	 * Make a request to Mailchimp API
+	 *
+	 * @param unknown $request
+	 * @param array $parameters
+	 * @param unknown $api_path
+	 * @return boolean|mixed
+	 */
 	private function make_request( $request, $parameters = array(), $api_path = null ) {
 		$result = '';
-		$url = $this->root;
+		$url = $this->root . $api_path;
 		$headers = array(
 			'Content-Type: application/json',
-			'Authorization: Basic ' . base64_encode( 'user:'. $this->apikey )
+			'Authorization: Basic ' . base64_encode( 'user:' . $this->apikey )
 		);
 
-		switch( $request ) {
+		switch ( $request ) {
 			case 'lists' :
 			case 'check' :
-				$url .= $api_path;
 				$ch = curl_init();
 				curl_setopt( $ch, CURLOPT_URL, $url );
 				curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
-				break;			
-			case 'add':
-				////if ( isset( $list_id ) ) {
-					$url .= $api_path;
-					$ch = curl_init( $url );
-					curl_setopt( $ch, CURLOPT_USERPWD, 'user:' . $this->apikey );
-					//curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'POST' );
-					curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
-					curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $parameters ) );
-					break;
-				//} else {
-				//	break;
-				//}
-			case 'update':
-				//if ( isset( $list_id ) && isset( $parameters['email_address'] ) ) {
-					$url .= $api_path;
-					$ch = curl_init( $url );
-					curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'PATCH' );
-					curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $parameters ) );
-				//}
+				break;
+			case 'add' :
+				$ch = curl_init( $url );
+				curl_setopt( $ch, CURLOPT_USERPWD, 'user:' . $this->apikey );
+				curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
+				curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $parameters ) );
+				break;
+			case 'update' :
+				$ch = curl_init( $url );
+				curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'PATCH' );
+				curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $parameters ) );
 				break;
 		}
 
@@ -198,20 +275,20 @@ class Mailchimp_Api {
 
 		$result     = curl_exec( $ch );
 		$httpd_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
-	    curl_close( $ch );
-	    if ( $httpd_code != '200' ) {
-	    	$result = false;
-	    } else {
-	    	$result = json_decode( $result, true );
-	    }
+		curl_close( $ch );
+		if ( $httpd_code != '200' ) {
+			$result = false;
+		} else {
+			$result = json_decode( $result, true );
+		}
 
-	    return $result;
+		return $result;
 	}
 
 	public function readConfigs() {
 		$paths = array( '~/.mailchimp.key', '/etc/mailchimp.key' );
-		foreach( $paths as $path ) {
-			if( file_exists( $path ) ) {
+		foreach ( $paths as $path ) {
+			if ( file_exists( $path ) ) {
 				$apikey = trim( file_get_contents( $path ) );
 				if ( $apikey ) {
 					return $apikey;
