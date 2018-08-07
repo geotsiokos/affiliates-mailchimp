c32b96c764b3e261be754feea40b0d66544795e1
geotsiokos <gtsiokos@gmail.com>
2018-08-07 17:06:55 +0300

    fixed license headers, added delete settings option

--- class-affiliates-mailchimp.php
+++ class-affiliates-mailchimp.php
@@ -4,9 +4,13 @@
  *
  * Copyright (c) 2018 www.itthinx.com
  *
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ *
  * This code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * This header and all notices must be kept intact.
  *
@@ -38,6 +42,7 @@ class Affiliates_MailChimp {
 		load_plugin_textdomain( 'affiliates-mailchimp', false, 'affiliates-mailchimp/languages' );
 		add_action( 'init', array( __CLASS__, 'wp_init' ) );
 		add_action( 'admin_notices', array( __CLASS__, 'admin_notices' ) );
+		register_uninstall_hook( __FILE__, array( __CLASS__, 'uninstall' ) );
 	}
 
 	/**
@@ -99,48 +104,46 @@ class Affiliates_MailChimp {
 			wp_die( esc_html__( 'Access denied.', 'affiliates-mailchimp' ) );
 		}
 		$options = get_option( 'affiliates-mailchimp' );
-	?>
-	<div class="wrap">
-	
-	<?php
-	if ( isset( $_POST['submit'] ) ) {
-		if ( wp_verify_nonce( $_POST['aff-mailchimp-nonce'], 'aff-mc-set-admin-options' ) ) {
-			$options['api_key']            = $_POST['api_key'];
-			$options['list_name']          = $_POST['list_name'];
-			$options['interests_category'] = $_POST['interests_category'];
-			$options['interest']           = $_POST['interest'];
-			$options['need_confirm']       = $_POST['need_confirm'];
+		?>
+		<div class="wrap">
 
+		<?php
+		if ( isset( $_POST['submit'] ) ) {
+			if ( wp_verify_nonce( $_POST['aff-mailchimp-nonce'], 'aff-mc-set-admin-options' ) ) {
+				$options['api_key']            = $_POST['api_key'];
+				$options['list_name']          = $_POST['list_name'];
+				$options['interests_category'] = $_POST['interests_category'];
+				$options['interest']           = $_POST['interest'];
+				$options['need_confirm']       = $_POST['need_confirm'];
+				$options['delete_settings']    = isset( $_POST['delete_settings'] ) ? 1 : 0;
+			}
+			update_option( 'affiliates-mailchimp', $options );
+		} elseif ( isset( $_POST['generate'] ) ) {
+			Affiliates_Mc::synchronize();
 		}
-		update_option( 'affiliates-mailchimp', $options );
 
-	} elseif ( isset( $_POST['generate'] ) ) {
+		$api_key            = isset( $options['api_key'] ) ? $options['api_key'] : null;
+		$list_name          = isset( $options['list_name'] ) ? $options['list_name'] : '';
+		$interests_category = isset( $options['interests_category'] ) ? $options['interests_category'] : '';
+		$interest           = isset( $options['interest'] ) ? $options['interest'] : '';
+		$need_confirm       = isset( $options['need_confirm'] ) ? $options['need_confirm'] : 0;
+		$delete_settings    = isset( $options['delete_settings'] ) ? $options['delete_settings'] : null;
 
-		Affiliates_Mc::synchronize();
-
-	}
-
-	$api_key            = isset( $options['api_key'] ) ? $options['api_key'] : null;
-	$list_name          = isset( $options['list_name'] ) ? $options['list_name'] : '';
-	$interests_category = isset( $options['interests_category'] ) ? $options['interests_category'] : '';
-	$interest           = isset( $options['interest'] ) ? $options['interest'] : '';
-	$need_confirm       = isset( $options['need_confirm'] ) ? $options['need_confirm'] : 0;
-
-	$description = '';
-	if ( !$api_key ) {
-		$description = esc_html__( 'Affiliates Mailchimp needs a valid API key to connect with MailChimp servers.', 'affiliates-mailchimp' );
-	}
-	?>
+		$description = '';
+		if ( !$api_key ) {
+			$description = esc_html__( 'Affiliates Mailchimp needs a valid API key to connect with MailChimp servers.', 'affiliates-mailchimp' );
+		}
+		?>
 
 	<h2>
-	<?php echo esc_html__( 'Affiliates MailChimp', 'affiliates-mailchimp' ); ?>
+		<?php echo esc_html__( 'Affiliates MailChimp', 'affiliates-mailchimp' ); ?>
 	</h2>
 	<form method="post" name="options" action="">
 		<table class="form-table">
 			<tr valign="top">
 			<th scope="row"><?php echo esc_html__( 'API Key:', 'affiliates-mailchimp' ); ?></th>
 			<td><input type="text" name="api_key" value="<?php echo esc_attr( $api_key ); ?>" />
-				<p class="description"><?php echo esc_html( $description ); //write_log( $options);?></p>
+				<p class="description"><?php echo esc_html( $description ); ?></p>
 			</td>
 			</tr>
 	
@@ -186,6 +189,13 @@ class Affiliates_MailChimp {
 				  </select>
 						<p class="description"><?php echo esc_html__( 'Control whether a double opt-in confirmation message is sent. Abusing this may cause your mailchimp account to be suspended.' , 'affiliates-mailchimp' ); ?></p>
 			  </tr>
+			  <tr>
+			  <th scope="row"><?php echo esc_html__( 'Delete Settings:', 'affiliates-mailchimp' ); ?></th>
+			  <td>
+			  <input type="checkbox" name="delete_settings" <?php echo ( esc_attr( $delete_settings ) == 1 ? ' checked="checked" ' : '' ); ?> />
+			  <p class="description"><?php echo esc_html__( 'CAUTION: If this option is active while the plugin is uninstalled, the above settings will be DELETED. If you want to keep these settings and are going to deactivate it, make sure to keep a note or backup or do not enable this option.' , 'affiliates-mailchimp' ); ?></p>
+			  </td>
+			  </tr>
 		</table>
 		<p>
 		<?php
@@ -214,6 +224,16 @@ class Affiliates_MailChimp {
 	</form>
 	</div>
 	<?php
+	}
+
+	/**
+	 * Option to delete plugin settings upon uninstall
+	 */
+	public static function uninstall() {
+		$options = get_option( 'affiliates-mailchimp' );write_log('deacti');
+		if ( isset( $options['delete_settings'] ) && $options['delete_settings'] == 1 ) {
+			delete_option( 'affiliates-mailchimp' );
+		}
 	}
 }
 Affiliates_MailChimp::init();
