15946e7c328669c89693b21ff7bf9a6a3dc56011
geotsiokos <gtsiokos@gmail.com>
2018-08-07 19:53:02 +0300

    added deactivation hook

--- affiliates-mailchimp.php
+++ affiliates-mailchimp.php
@@ -36,5 +36,13 @@ define( 'AFFILIATES_MAILCHIMP_PLUGIN_DOMAIN', 'affiliates-mailchimp' );
 define( 'AFFILIATES_MAILCHIMP_FILE', __FILE__ );
 define( 'AFFILIATES_MAILCHIMP_CORE_DIR', WP_PLUGIN_DIR . '/affiliates-mailchimp' );
 
+register_deactivation_hook( __FILE__, 'deactivate' );
+function deactivate() {
+	$options = get_option( 'affiliates-mailchimp' );write_log('deacti');
+	if ( isset( $options['delete_settings'] ) && $options['delete_settings'] == 1 ) {
+		delete_option( 'affiliates-mailchimp' );
+	}
+}
+
 require_once 'class-affiliates-mailchimp.php';
 require_once 'class-affiliates-mc.php';
